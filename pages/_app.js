e911bf7f7a155db6aca4c10ecee01613332ce22c
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-05 12:10:36 -0400

    made changes to app.js, index and css files to fix errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,22 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NoAuth from '../components/NoAuth'; // TODO: COMMENT OUT FOR AUTH
 import '../styles/globals.css';
-// import { AuthProvider } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector'; // TODO: COMMENT IN FOR AUTH
+import { AuthProvider } from '../utils/context/authContext';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NoAuth
+    <AuthProvider>
+      {' '}
+      {/* gives children components access to user and auth methods */}
+      <ViewDirectorBasedOnUserAuthStatus
+        // if status is pending === loading
+        // if status is logged in === view app
+        // if status is logged out === sign in page
         component={Component}
         pageProps={pageProps}
       />
-
-      {/* TODO: Delete NoAuth component above and comment in code below for authentication */}
-      {/* <AuthProvider>
-        <ViewDirectorBasedOnUserAuthStatus
-          // if status is pending === loading
-          // if status is logged in === view app
-          // if status is logged out === sign in page
-          component={Component}
-          pageProps={pageProps}
-        />
-      </AuthProvider> */}
-    </>
+    </AuthProvider>
   );
 }
 
