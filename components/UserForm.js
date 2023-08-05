41a678582551977cb34d5b17baeb8723f4f2068f
Stacey Vanyo <sbvanyo@gmail.com>
2023-08-05 13:52:52 -0500

    user form edits

--- components/UserForm.js
+++ components/UserForm.js
@@ -39,7 +39,55 @@ function UserForm() {
             type="text"
             placeholder="Enter First Name"
             name="first_name"
-            value={formInput.title}
+            value={formInput.first_name}
+            onChange={handleChange}
+            required
+          />
+        </FloatingLabel>
+
+        {/* LAST NAME INPUT  */}
+        <FloatingLabel controlId="floatingInput1" label="Last Name" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Enter Last Name"
+            name="last_name"
+            value={formInput.last_name}
+            onChange={handleChange}
+            required
+          />
+        </FloatingLabel>
+
+        {/* PHONE NUMBER INPUT  */}
+        <FloatingLabel controlId="floatingInput1" label="Phone Number" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Enter Phone Number"
+            name="phone_number"
+            value={formInput.phone_number}
+            onChange={handleChange}
+            required
+          />
+        </FloatingLabel>
+
+        {/* EMAIL INPUT  */}
+        <FloatingLabel controlId="floatingInput1" label="Email" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            value={formInput.email}
+            onChange={handleChange}
+            required
+          />
+        </FloatingLabel>
+
+        {/* ADDRESS INPUT  */}
+        <FloatingLabel controlId="floatingInput1" label="Address" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Enter Address"
+            name="address"
+            value={formInput.address}
             onChange={handleChange}
             required
           />
