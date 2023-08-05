b68fe4df816ffa94521c54c7a0873d07b069ddad
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-05 11:41:13 -0400

    brought over auth changes from E21 front-end template

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import Link from 'next/link';
 import {
-  Navbar, Container, Nav, Button,
+  Navbar, //
+  Container,
+  Nav,
+  Button,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
-export default function NavBarAuth() {
+export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -20,7 +23,12 @@ export default function NavBarAuth() {
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
             </Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            <Link passHref href="/delete-me">
+              <Nav.Link>Delete Me</Nav.Link>
+            </Link>
+            <Button variant="danger" onClick={signOut}>
+              Sign Out
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
