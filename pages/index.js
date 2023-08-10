905d7edb15f419d2d5d2812a748b947519bae866
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-10 18:32:51 -0400

    adjusted homepage with logo, welcome message, and buttons

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import Link from 'next/link';
 import { useAuth } from '../utils/context/authContext';
+import Logo from '../components/Logo';
 
 function Home() {
   const { user } = useAuth();
 
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <Logo />
+        <h1>Welcome, {user.first_name}! </h1>
+
+        <Link passHref href="/menu/menuPage">
+          <Button className="menu-btn">Menu</Button>
+        </Link>
+
+        <Link passHref href="/cart">
+          <Button className="cart-btn">Current Order</Button>
+        </Link>
+
+        <Link passHref href="/orders">
+          <Button className="past-orders-btn">View Past Orders</Button>
+        </Link>
+      </div>
+    </>
   );
 }
 
